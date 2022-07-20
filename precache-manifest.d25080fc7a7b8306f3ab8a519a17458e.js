a2dc13fe7410b6e1a15357ed2a42ee86defe3cc6
Vladimir <vpprilipukhov@gmail.com>
2022-07-21 02:28:58 +0300

    end

---
+++ precache-manifest.d25080fc7a7b8306f3ab8a519a17458e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0146195d36a6a023b508",
+    "url": "/static/css/main.e0f78eb8.chunk.css"
+  },
+  {
+    "revision": "0146195d36a6a023b508",
+    "url": "/static/js/main.0146195d.chunk.js"
+  },
+  {
+    "revision": "301fa55c94ec007b5004",
+    "url": "/static/js/1.301fa55c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "573788e661ee330c36948d04a07fa09b",
+    "url": "/static/media/Fragment_fona-07-07.573788e6.png"
+  },
+  {
+    "revision": "f6adf06554c7c68c9801796508f75333",
+    "url": "/static/media/shield.f6adf065.png"
+  },
+  {
+    "revision": "7a1f00c4b91a7b90c5007e6b7b136ff1",
+    "url": "/static/media/finish.7a1f00c4.png"
+  },
+  {
+    "revision": "9b25958f01444b39bde66dc8a67632e0",
+    "url": "/index.html"
+  }
+];
